c7343ee7f1c84656a79c16c943312360979fb7d3
rkudik001c <RamaKrishna_Kudikala@cable.comcast.com>
2019-08-26 16:15:09 -0400

    test with axios

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,26 @@
-import http from 'http';
+// import http from 'http';
+import axios from 'axios';
 
+// const getResponseData = (url) => new Promise((resolve, reject) => {
+//     http.get(url, res => {
+//         const { statusCode } = res;
+//         const contentType = res.headers['content-type'];
 
-const getResponseData = (url) => new Promise((resolve, reject) => {
-    http.get(url, res => {
-        const { statusCode } = res;
-        const contentType = res.headers['content-type'];
-
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => resolve({ statusCode, contentType, rawData }));
-    }).on('error', e => reject(e));
-});
+//         res.setEncoding('utf8');
+//         let rawData = '';
+//         res.on('data', (chunk) => { rawData += chunk; });
+//         res.on('end', () => resolve({ statusCode, contentType, rawData }));
+//     }).on('error', e => reject(e));
+// });
 
 fixture `Test REST API`;
 
 test('endpoint must return 200', async t => {
-    const response = await getResponseData('https://www.xfinity.com/healthcheck');
-    await t
-        .expect(response.statusCode).eql(200)
+    // const response = await getResponseData('https://www.xfinity.com/healthcheck');
+    // await t
+    //     .expect(response.statusCode).eql(200)
+
+    const response = await axios.get("https://www.xfinity.com/healthcheck");
+    await t.expect(response.status).eql(200);
 });
 
